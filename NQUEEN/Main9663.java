97d7cd3e60e6c967bc42cd36c04347cf03fd9c6d
iohyeon <76993366+iohyeon@users.noreply.github.com>
2022-08-17 21:30:28 +0900

    N-Queen
    재귀호출

---
+++ NQUEEN/Main9663.java
@@ -0,0 +1,13 @@
+package study.NQUEEN;
+
+import java.io.IOException;
+
+public class Main9663 {
+    static int N, M;
+    static int[] arr, sel;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
